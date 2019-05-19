9fe06129e44e43f56187dc67474db80a93f37113
jihunhong <redgee49@gmail.com>
2019-05-19 16:24:46 +0900

    View 중간 단계 완성 & 리소스( css, image)  추가

--- src/main/java/com/cafe24/demo/Controller/HomeController.java
+++ src/main/java/com/cafe24/demo/Controller/HomeController.java
@@ -19,20 +19,41 @@ class HomeController {
 
     JsoupUtils utils = new JsoupUtils();
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = {"/","/melon"})
     public String Home(Model model){
 
         List<Music> melon = new ArrayList<>();
-        List<Music> genie = new ArrayList<>();
-        List<Music> bugs = new ArrayList<>(); 
         
         melon = utils.getMelonChart();
-        genie = utils.getGeineChart();
-        bugs  = utils.getBugsChart();
 
-        model.addAttribute("melon", melon);
-        model.addAttribute("genie", genie);
-        model.addAttribute("bugs", bugs);
+        model.addAttribute("list", melon);
+        model.addAttribute("name", "melon");
+        
+        return "index";
+    }
+
+    @RequestMapping(value = "/genie")
+    public String Genie(Model model){
+
+        List<Music> genie = new ArrayList<>();
+
+        genie = utils.getGeineChart();
+
+        model.addAttribute("list", genie);
+        model.addAttribute("name", "genie");
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/bugs")
+    public String Bugs(Model model){
+
+        List<Music> bugs = new ArrayList<>();
+
+        bugs = utils.getBugsChart();
+
+        model.addAttribute("list", bugs);
+        model.addAttribute("name", "bugs");
 
         return "index";
     }
