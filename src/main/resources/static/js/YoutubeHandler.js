e33c6498da45ea8ea250f24e4004f02e8e5de0a0
jihunhong <redgee49@gmail.com>
2019-07-02 18:55:51 +0900

    Preview Top Music Header 수정

--- src/main/resources/static/js/YoutubeHandler.js
+++ src/main/resources/static/js/YoutubeHandler.js
@@ -41,4 +41,40 @@ window.onload = function(){
     for (var i = 0 ; i < youtube_link.length; i++) {
         youtube_link[i].addEventListener('click' , event_handler , false ) ; 
     }
+
+    $('.top_music').on('click', function(){
+        var query = this.innerText;
+        
+        var title = $(this).find('.top_title').text();
+        var artist = $(this).find('.top_author').text();
+
+        $.ajax({
+            url: "/youtube/api/search?q=" + encodeURIComponent(query),
+            type: 'GET',
+            cache : false,
+            dataType : "json",
+            success:function(result, textStatus, xhr, response){
+
+                console.log(title + artist);
+
+                var official = result[0];
+
+                for(var i=2; i>=0; i--){
+                    if( result[i].title.includes("official") || result[i].title.includes("MV") ||  result[i].title.includes("M/V")){
+                        official = result[i];
+                    }
+                }
+                
+
+                document.getElementsByClassName("modal-title")[0].innerHTML = title + "  -  " + artist;
+
+                document.getElementsByClassName("embed-responsive embed-responsive-16by9 z-depth-1-half")[0].innerHTML = "<iframe width='360' height='200' src='https://www.youtube.com/embed/"
+                                                                             + official.url 
+                                                                             + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>"
+                                                                             + "</iframe>";
+
+                
+            }
+        });
+    });
 }
